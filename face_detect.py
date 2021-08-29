59c685542051492bba0fa47039139bc9fb3f9d7a
Ethol Palmer <ethol.palmer@gmail.com>
2021-08-28 23:05:18 -0400

    Completed Unit tests on the classes created and documented a challenge I had with one file

--- face_detect.py
+++ face_detect.py
@@ -6,48 +6,51 @@ import cv2
 # from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 
+import face_detector as fdd
+
 class Face_Detect:
     def __init__(self
             , file_name
             , confidence=0.5
-            , proto="face_detector/deploy.prototxt"
-            , wghts="face_detector/res10_300x300_ssd_iter_140000.caffemodel") -> None:
-
+            , net_mdl=fdd.Face_Detector.Get_Mdl()) -> None:
+        image_failed = False
         self.file_name            = file_name if os.path.exists(file_name) else None
         self.image                = cv2.imread(self.file_name)
-        self.results              = {}        
+        self.results              = {}
         (self.height, self.width) = self.image.shape[:2]
+        self.net_mdl              = net_mdl
+
         print(f"The file ({self.file_name}) exists => {os.path.exists(self.file_name)}")
 
-        @classmethod
         def get_face(X_begin, Y_begin, X_end, Y_end):
-            if self.image is not None:
-                face = self.image[Y_begin:Y_end, X_begin:X_end]
-                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-                face = cv2.resize(src=face,dsize=(224,224))
-                face = img_to_array(face)
-                return np.expand_dims(face, axis=0)
-            else:
+            try:
+                if self.image is not None:
+                    face = self.image[Y_begin:Y_end, X_begin:X_end]
+                    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                    face = cv2.resize(src=face,dsize=(224,224))
+                    face = img_to_array(face)
+                    return np.expand_dims(face, axis=0)
+                else:
+                    image_failed = True
+                    return None
+            except Exception as excn:
+                image_failed = True
                 return None
-        @classmethod
-        def get_measurements(poss_faces):
+            else:
+                return face
 
+        def get_measurements(poss_faces):
             outline = poss_faces[0,0,x, 3:7] * np.array([self.width,self.height,self.width,self.height])
             X_begin, Y_begin, X_end, Y_end = outline.astype("int")
-            
+
             return max(0,X_begin), max(0, Y_begin), min(self.width -1, X_end), min(self.height-1, Y_end)
 
-        # Face Detector
-        p = os.path.altsep.join(['.',proto])
-        w = os.path.altsep.join(['.',wghts])
-        print(os.path.exists(p),os.path.exists(w))
-        net_mdl = cv2.dnn.readNet(p,w)
         # self.image = cv2.imread(filename=self.file_name)
         fixed_param = (104.0, 177.0, 123.0)
         img_blob = cv2.dnn.blobFromImage(self.image,1,(300,300), fixed_param)
-        net_mdl.setInput(img_blob)
+        self.net_mdl.setInput(img_blob)
 
-        possible_faces = net_mdl.forward()
+        possible_faces = self.net_mdl.forward()
 
         faces_info = []
         # for each face detected with a confidence level
@@ -55,10 +58,17 @@ class Face_Detect:
         for x in range(possible_faces.shape[2]):
             conf = possible_faces[0,0,x,2]
             if conf > confidence:
-                x1,y1,x2,y2 = get_measurements(self, poss_faces=possible_faces)
-                faces_info.append((get_face(self, x1,y1,x2,y2),conf))
+                x1,y1,x2,y2 = get_measurements(poss_faces=possible_faces)
+                f = get_face(x1,y1,x2,y2)
+                if f is not None:
+                    faces_info.append((f,conf))
 
-        self.results[file_name]=faces_info
+        if not image_failed:
+            self.results[file_name]=faces_info
+        else:
+            print(f"Failed to process image:{file_name}")
+            self.results[file_name]=None
+
     def __del__(self):
         print("Exiting Face Detect Class.")
         del self.image
@@ -67,4 +77,5 @@ if __name__ == "__main__":
     file1 = Face_Detect("./dataset/with_mask/100-with-mask.jpg")
 
     for fi in file1.results[file1.file_name]:
-        print(fi[0].shape)
+        if fi[0] is not None:
+            print(fi[0].shape)
---
+++ face_detector.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+
+class Face_Detector:
+    def __init__(self) -> None:
+            # , proto="face_detector/deploy.prototxt"
+            # , wghts="face_detector/res10_300x300_ssd_iter_140000.caffemodel") -> None:
+        # # Configure Face Dector Model
+        # p = os.path.altsep.join(['.',proto])
+        # w = os.path.altsep.join(['.',wghts])
+        # if (os.path.exists(p) and os.path.exists(w)):
+        #     self.net_mdl = cv2.dnn.readNet(p,w)
+        # else:
+        #     self.net_mdl = None
+        pass
+
+    @staticmethod
+    def Get_Mdl(proto="face_detector/deploy.prototxt"
+            , wghts="face_detector/res10_300x300_ssd_iter_140000.caffemodel"):
+        # Configure Face Dector Model
+        p = os.path.altsep.join(['.',proto])
+        w = os.path.altsep.join(['.',wghts])
+        if (os.path.exists(p) and os.path.exists(w)):
+            return cv2.dnn.readNet(p,w)
+        else:
+            return None
+
+    def __del__(self):
+        print("Face_Detector Exited.")
+
+if __name__ == "__main__":
+    mdl = Face_Detector().Get_Mdl()
+    img = cv2.imread("./dataset/with_mask/100-with-mask.jpg")
+    fixed_param = (104.0, 177.0, 123.0)
+    img_blob = cv2.dnn.blobFromImage(img,1,(300,300), fixed_param)
+    mdl.setInput(img_blob)
+    print(mdl.forward())
